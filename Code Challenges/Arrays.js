49628672688605f8285269360f5f1b2a838f5ff0
mrchappell <michael.r.chappell@gmail.com>
2020-02-14 23:38:11 -0600

    code challenge week 2

---
+++ Code Challenges/Arrays.js
@@ -0,0 +1,32 @@
+const socialMedia = ["Facebook", "Instagram", "Twitter"];
+
+socialMedia.unshift("Tumblr", "Goodreads");
+
+
+
+console.log(socialMedia);
+
+
+
+ ["Tumblr", "Goodreads", "Facebook", "Instagram", "Twitter"]
+
+ //push(): 
+
+const Obj = {
+
+              arrayOne: [],
+
+              arrayTwo: []
+
+};
+
+
+
+const languages = ['Spanish', 'English', 'German];
+
+Obj.arrayOne.push(arrayOne);
+
+
+
+alert(Obj.arrayOne);
+
